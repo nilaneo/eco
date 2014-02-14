d84e4375771b06bab71e8065b92f55c5e332f2f3
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2014-02-14 16:07:35 +0200

    Make list in the purse and inputs

--- js/main.js
+++ js/main.js
@@ -18,18 +18,4 @@ $(function() {
 		if( t.val().length==0 ) t.val( t.attr('def') )
 	});
 
-	/*
-	* SLIDERS
-	*/
-
-	var slides = [$(".slide-1"), $(".slide-2"), $(".slide-3")];
-
-	$(".btn-next").on("click", function(e) {
-		e.preventDefault();
-		slides[0].addClass("is-maximized");
-		slides[1].removeClass("is-minified");
-		slides[2].removeClass("is-maximized").addClass("is-minified");
-		slides.push(slides.shift());
-	});
-
 });
